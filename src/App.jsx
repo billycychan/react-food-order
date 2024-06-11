892c618b77fcc7446dc147b148521f57fde739ba
Billy Chan <billycychan.work@gmail.com>
2024-06-11 18:30:33 -0400

    feat: added Modal Cart, open and close

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Header from "./components/Headers";
 import Meals from "./components/Meals";
+import Cart from "./components/Cart";
+
 import { CartContextProvider } from "./store/CartContext.jsx";
+import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
 
 function App() {
 	return (
-		<CartContextProvider>
-			<Header />
-			<Meals />
-		</CartContextProvider>
+		<UserProgressContextProvider>
+			<CartContextProvider>
+				<Header />
+				<Meals />
+				<Cart />
+			</CartContextProvider>
+		</UserProgressContextProvider>
 	);
 }
 
--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,11 +1,15 @@
+import { useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
+
 export default function Modal({ children, open, className }) {
 	const dialog = useRef();
 
 	useEffect(() => {
+		const modal = dialog.current;
 		if (open) {
-			dialog.current.showModal();
+			modal.showModal();
 		}
+		return () => modal.close();
 	}, [open]);
 
 	return createPortal(
